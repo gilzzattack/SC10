404e8326894561d86d725558a380240aaf0fcc64
gilzzattack <117437673+gilzzattack@users.noreply.github.com>
2022-11-04 22:26:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,14 +16,14 @@ global.xteam = 'apivproject'
 global.lolhuman = 'yourkey'
 
 // Other
-global.owner = ['6281511480762']
-global.premium = ['6281511480762']
-global.ownername = 'YahyaGanzz'
-global.botname = 'AzzBot•MD'
-global.packname = '© YahyaGanzz'
-global.gc = 'https://github.com/AzzBott679'
+global.owner = ['6285730794089']
+global.premium = ['6285730794089']
+global.ownername = 'Gilzz'
+global.botname = 'Gilzz - OXD'
+global.packname = '© Gilzz Ofc'
+global.gc = 'https://chat.whatsapp.com/CxJShadfxYqCVAHHxdHSiy'
 global.linkyt = 'https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g'
-global.linkgc = 'https://chat.whatsapp.com/CkguID8hSdSFAaBrKNHLSr'
+global.linkgc = 'https://chat.whatsapp.com/CxJShadfxYqCVAHHxdHSiy'
 global.limitawal = '10'
 global.author = '@yahyaganzz\n@AzzBot'
 global.sessionName = 'sessionya'
